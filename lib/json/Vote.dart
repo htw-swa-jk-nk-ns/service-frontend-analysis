34514004e54df4f77ee6b1bb4a8b6a70690b42cb
Nicolas Klein <nicolasklein1998@gmail.com>
2020-12-08 14:16:00 +0100

    Now fetched multiple items form sample rest api

---
+++ lib/json/Vote.dart
@@ -0,0 +1,52 @@
+class Vote {
+  String country = null;
+
+  String name = null;
+
+  int voteFor = null;
+
+  String date = null;
+
+  int id = null;
+
+  Vote();
+
+  @override
+  String toString() {
+    return 'Vote[country=$country, name=$name, voteFor=$voteFor, date=$date, id=$id, ]';
+  }
+
+  Vote.fromJson(Map<String, dynamic> json) {
+    if (json == null) return;
+    country = json['country'];
+    name = json['name'];
+    voteFor = json['voteFor'];
+    date = json['date'];
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'country': country,
+      'name': name,
+      'voteFor': voteFor,
+      'date': date,
+      'id': id
+    };
+  }
+
+  static List<Vote> listFromJson(List<dynamic> json) {
+    return json == null
+        ? new List<Vote>()
+        : json.map((value) => new Vote.fromJson(value)).toList();
+  }
+
+  static Map<String, Vote> mapFromJson(Map<String, Map<String, dynamic>> json) {
+    var map = new Map<String, Vote>();
+    if (json != null && json.length > 0) {
+      json.forEach((String key, Map<String, dynamic> value) =>
+          map[key] = new Vote.fromJson(value));
+    }
+    return map;
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,18 +13,21 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Future<Album> _futureAlbum;
+  Future<List<Album>> _futureAlbums;
   int index = 1;
 
   @override
   void initState() {
     super.initState();
-    setUpTimedFetch();
+    //setUpTimedFetch();
+    reload();
   }
 
   void reload() {
     setState(() {
       index = index + 1;
-      _futureAlbum = fetchAlbum();
+      //_futureAlbum = fetchAlbum();
+      _futureAlbums = fetchAlbums();
     });
   }
 
@@ -43,7 +46,7 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Voting-Analysis'),
       ),
-      body: getFutureBuilder(),
+      body: getFutureBuilderAlbums(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => reload(),
         tooltip: 'Increment',
@@ -68,12 +71,52 @@ class _HomeState extends State<Home> {
     }
   }
 
-  FutureBuilder<Album> getFutureBuilder() {
+  Future<List<Album>> fetchAlbums() async {
+    final response =
+        await http.get('https://jsonplaceholder.typicode.com/albums');
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+
+      // store json data into list
+      var list = json.decode(response.body) as List;
+
+      // iterate over the list and map each object in list to Img by calling Img.fromJson
+      List<Album> albums = list.map((i) => Album.fromJson(i)).toList();
+
+      print(albums.runtimeType); //returns List<Img>
+      print(albums[0].runtimeType); //returns Img
+
+      return albums;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
+  FutureBuilder<Album> getFutureBuilderAlbum() {
     return FutureBuilder<Album>(
       future: _futureAlbum,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return Text(snapshot.data.title);
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  FutureBuilder<List<Album>> getFutureBuilderAlbums() {
+    return FutureBuilder<List<Album>>(
+      future: _futureAlbums,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data[index].title);
         } else if (snapshot.hasError) {
           return Text("${snapshot.error}");
         }
