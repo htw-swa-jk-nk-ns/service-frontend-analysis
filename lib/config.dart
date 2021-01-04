add969cbb8491fa6719e13be47f85ae678f9a6e4
Nicolas Klein <nicolasklein1998@gmail.com>
2021-01-04 14:11:42 +0100

    URLs now reflect kubernetes cluster - standard subdomain

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,4 @@
-const String URL_RESULTS = 'http://localhost:3000/results';
-const String URL_RESULTS_COUNTRY = 'http://localhost:3000/country';
+const String URL_RESULTS = 'http://service-serving-layer-service:8081/results';
+const String URL_RESULTS_COUNTRY =
+    'http://service-serving-layer-service:8081/country';
 const int FETCH_INTERFAL = 4000;
