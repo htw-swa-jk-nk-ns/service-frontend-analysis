d14ff928d9db76fa8e9494e53ffd795ae66bd4cd
Nicolas Klein <nicolasklein1998@gmail.com>
2021-01-05 13:17:06 +0100

    Switched URLs to refelct serving layer setup

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,5 @@
-const String URL_RESULTS = 'http://service-serving-layer-service:8081/results';
+const String URL_RESULTS =
+    'http://service-serving-layer-service:8081/getResults';
 const String URL_RESULTS_COUNTRY =
-    'http://service-serving-layer-service:8081/country';
+    'http://service-serving-layer-service:8081/getResultsByCountry';
 const int FETCH_INTERFAL = 4000;
