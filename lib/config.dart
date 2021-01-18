f6904ec804a7bdb4206fc7c92c5cc6d008d50ffc
Nicolas Klein <nicolasklein1998@gmail.com>
2021-01-18 11:18:19 +0100

    Switched to voting-app.com endpoint

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,3 @@
-const String URL_RESULTS = 'http://service-serving-layer:8080/results';
-const String URL_RESULTS_COUNTRY =
-    'http://service-serving-layer:8080/votesByCountry';
+const String URL_RESULTS = 'http://voting-app.com/results';
+const String URL_RESULTS_COUNTRY = 'http://voting-app.com/votesByCountry';
 const int FETCH_INTERFAL = 5000;
