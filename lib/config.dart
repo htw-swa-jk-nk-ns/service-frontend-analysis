1740a27695951b8196c38abcac6f09a8297064c5
Nicolas Klein <nicolasklein1998@gmail.com>
2021-01-19 15:56:45 +0100

    URL now votesByCountry

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
 const String URL_RESULTS = 'http://voting-app.com/all';
-const String URL_RESULTS_COUNTRY = 'http://voting-app.com/country';
+const String URL_RESULTS_COUNTRY = 'http://voting-app.com/votesByCountry';
 const int FETCH_INTERFAL = 5000;
