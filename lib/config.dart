d26955884915efa06481e88d14d6f5d6064eb804
Nicolas Klein <nicolasklein1998@gmail.com>
2021-01-25 14:52:47 +0100

    Switched urls

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
-const String URL_RESULTS = 'http://voting-app.com/all';
+const String URL_RESULTS = 'http://voting-app.com/results';
 const String URL_RESULTS_COUNTRY = 'http://voting-app.com/votesByCountry';
 const int FETCH_INTERFAL = 5000;
